6fd4e150b43bfe0519710274c043c57dee0c761f
havzerone <havzerone@localhost.localdomain>
2022-07-12 13:40:16 +0300

    added main.cpp and CmakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int arsc, char* argv[])
+{
+    std::cout << "Hello World! My name is HavZerOne, this is my (first) project on github with VSCode + Cmake on Linux! Have a nice day and good luch for me!" << std::endl;
+    return 0;
+}
